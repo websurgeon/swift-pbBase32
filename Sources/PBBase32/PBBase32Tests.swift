d1ab115d2727a02110b586eb01e512e086695f14
Peter Barclay <peter@peter-barclay.com>
2018-03-15 22:20:27 +0000

    add unit test target for macOS

---
+++ Sources/PBBase32/PBBase32Tests.swift
@@ -0,0 +1,14 @@
+//
+//  Copyright © 2018 Peter Barclay. All rights reserved.
+//
+
+import XCTest
+
+class PBBase32Tests: XCTestCase {
+    
+    func test_example() {
+//        XCTFail("test failure")
+        XCTAssertTrue(true)
+    }
+    
+}
