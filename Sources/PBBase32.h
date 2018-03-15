b726dccf6e1d8cb44c6528e1260b1f28c6d9a998
Peter Barclay <peter@peter-barclay.com>
2018-03-15 09:22:40 +0000

    move target specific files to Sources
    - rename platform specific files Info.plist to Info-<platform>.plist
    - move plist files to /Sources
    - update target build settings to point to new location for plist files
    - uncheck target membership for plist files
    - replace platform specific project header files with /Sources/PBBase32.h
    - remove platform specific folders
    - change target Product Name to ${PROJECT_NAME} in build settings

---
+++ Sources/PBBase32.h
@@ -0,0 +1,11 @@
+//
+//  Copyright © 2018 Peter Barclay. All rights reserved.
+//
+
+@import Foundation;
+
+FOUNDATION_EXPORT double PBBase32_VersionNumber;
+
+FOUNDATION_EXPORT const unsigned char PBBase32_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PBBase32_iOS/PublicHeader.h>
