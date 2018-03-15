0505ffb85087820841f365e334628825bd0a32bf
Peter Barclay <peter@peter-barclay.com>
2018-03-15 08:45:00 +0000

    add target PBBase32-macOS

---
+++ PBBase32-macOS/PBBase32_macOS.h
@@ -0,0 +1,18 @@
+//
+//  PBBase32_macOS.h
+//  PBBase32-macOS
+//
+//  Created by Peter on 15/03/2018.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PBBase32_macOS.
+FOUNDATION_EXPORT double PBBase32_macOSVersionNumber;
+
+//! Project version string for PBBase32_macOS.
+FOUNDATION_EXPORT const unsigned char PBBase32_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PBBase32_macOS/PublicHeader.h>
+
+
