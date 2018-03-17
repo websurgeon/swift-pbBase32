526f7e821e6bb218489b382285eddaae8f16a350
Peter Barclay <peter@peter-barclay.com>
2018-03-17 18:23:38 +0000

    refactor paddingForInput

--- Sources/PBBase32/Encoder/Base32Encoder/Base32Encoder+Padding.swift
+++ Sources/PBBase32/Encoder/Base32Encoder/Base32Encoder+Padding.swift
@@ -4,25 +4,13 @@
 
 import Foundation
 
+private let paddingMap = [ 0: 0, 8: 6, 16: 4, 24: 3, 32: 1 ]
+
 extension Base32Encoder {
 
     func paddingForInput(size: Int, paddingChar: String) -> [Byte] {
         let blockSize = (size * 8) % 40
-        let padding: String
-        switch blockSize {
-        case 0:
-            padding = ""
-        case 8:
-            padding = String(repeating: paddingChar, count: 6)
-        case 16:
-            padding = String(repeating: paddingChar, count: 4)
-        case 24:
-            padding = String(repeating: paddingChar, count: 3)
-        case 32:
-            padding = String(repeating: paddingChar, count: 1)
-        default:
-            fatalError("invalid block size \(blockSize)")
-        }
+        let padding = String(repeating: paddingChar, count: paddingMap[blockSize]!)
         let paddingData = padding.data(using: .ascii)!
         return [Byte](paddingData)
     }
